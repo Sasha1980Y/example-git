65630193669ac9cd02b5aaf701b6039bb2cfe573
Alexandr <yakovenko5893@gmail.com>
2017-08-10 16:21:03 +0300

    Initial commit

---
+++ ForGitHub/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ForGitHub
+//
+//  Created by AlexanderYakovenko on 8/10/17.
+//  Copyright © 2017 AlexanderYakovenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // initial commit
+        let commit = "initial commit"
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
